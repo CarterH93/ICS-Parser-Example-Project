88bd9469853ce74a3a48f5ecb801fa07bb561db8
CarterH93 <Carterhawkins93@gmail.com>
2022-11-26 11:33:53 -0500

    Initial Commit

---
+++ ICS Parser Example Project/ICS_Parser_Example_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ICS_Parser_Example_ProjectApp.swift
+//  ICS Parser Example Project
+//
+//  Created by Carter Hawkins on 11/26/22.
+//
+
+import SwiftUI
+
+@main
+struct ICS_Parser_Example_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
